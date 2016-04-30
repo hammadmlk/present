717af1a3df4ab30f3f861a4a30d0e0b9587d6090
Malik <hammadm@amazon.com>
2016-04-30 15:22:12 -0400

    Basic HTML/CSS wireframe
    Wireframe HTML/CSS all one place.
    Next step should be to component-ize this.

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,16 @@
 var React = require('react');
 
+
+var mainStyle = {
+	height: '100%',
+	width: '100%',
+	backgroundColor: 'rgba(0, 0, 0, 0.1)'
+}
+
 var Main = React.createClass({
   render: function () {
     return (
-      <div className='main-container'>
+      <div className='main-container' style={mainStyle}>
         {this.props.children}
       </div>
     )
