f53defb70b1b5fc442e5803c2a84d75d5b401325
Malik <hammadm@amazon.com>
2016-05-13 19:11:13 -0400

    Main component scss style

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,15 +1,12 @@
+// This wraps the entire Application. Perhaps a better name will be AppContainer
 var React = require('react');
 
-var mainStyle = {
-	height: '100%',
-	width: '100%',
-	backgroundColor: 'rgba(0, 0, 0, 0.1)'
-}
+require('../scss/Main.scss');
 
 var Main = React.createClass({
   render: function () {
     return (
-      <div className='main-container' style={mainStyle}>
+      <div className='main' >
         {this.props.children}
       </div>
     )
