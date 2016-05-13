864f195592d8fd1f6aeff35fe4c230628ce6764a
Malik <hammadm@amazon.com>
2016-05-13 19:08:59 -0400

    adding sass-loader and extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
+var ExtractTextPluginConfig = new ExtractTextPlugin('style.css', {
+  allChunks: true
+})
 
 module.exports = {
   entry: [
@@ -15,9 +20,11 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"},
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      { test: /\.js$/, include: __dirname + '/app', loader: "babel-loader" },
+      //{ test: /\.css$/, loader: "style-loader!css-loader" },
+      //{ test: /\.scss$/, loaders: ['style', 'css', 'sass'] }
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass') }
     ]
   },
-  plugins: [HTMLWebpackPluginConfig]
+  plugins: [HTMLWebpackPluginConfig, ExtractTextPluginConfig]
 };
