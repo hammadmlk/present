f1fb3e46af64b56f799bc97f265ed54702e97889
Malik <hammadm@amazon.com>
2016-05-13 20:48:50 -0400

    Slide component update
    css in separate files
    style tweeks
    file formating

--- app/components/Slide.js
+++ app/components/Slide.js
@@ -3,45 +3,37 @@ var PropTypes = React.PropTypes;
 var SlideTitle = require('./SlideTitle');
 var SlideBody = require('./SlideBody');
 
-var slideStyle = {
-	display: 'flex',
-	flex: '0 0 100%',
+require('../scss/Slide.scss');
+
+// rotates between 1 - 6
+function getSlideColorNum(id) {
+    var numOfColors = 6;
+    return Math.abs(id % numOfColors) + 1;
 }
 
-var leftColumnStyle = {
-	display: 'flex',
-	flexGrow: 1,
-	flexBasis: '0px',
-	backgroundColor: 'rgba(222, 0, 0, 0.1)'
-};
+function Slide(props) {
 
-var rightColumnsStyle = {
-	display:'flex',
-	flexGrow: 3,
-	flexBasis: '0px',
-	backgroundColor: 'rgba(0, 0, 0, 0.1)'
+    var slideClassName = "slide slide-color-" + getSlideColorNum(props.id);
 
-};
-
-function Slide (props) {
-
-  return (
-  		<div className="slide" style={slideStyle} > 
-  			<div className="leftColumn" style={leftColumnStyle}> 
-		      	<SlideTitle title={props.title}/>
-	      	</div>
-	      	<div className="rightColumn" style={rightColumnsStyle}> 
-	      		<SlideBody />
-	      	</div>
-  	  	</div>
-  	)
+    return (
+        <div className={ slideClassName }>
+          <div className="left-column">
+            <SlideTitle
+                        title={ props.title }
+                        subTitle={ props.subTitle } />
+          </div>
+          <div className="right-column">
+            <SlideBody bulletList={ props.bulletList } />
+          </div>
+        </div>
+    )
 }
 
 Slide.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  subTitle: PropTypes.string,
-  bulletList: PropTypes.array.isRequired
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string,
+    bulletList: PropTypes.array.isRequired // validate each object in array is a bullet
 }
 
 module.exports = Slide;
